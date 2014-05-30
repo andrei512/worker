53a442997c61825b49ce879fad38961080dd5f38
Andrei Puni <puni.andrei23@gmail.com>
2014-05-31 00:01:39 +0300

    refactoring: moved server code to Worker module

--- app.rb
+++ app.rb
@@ -2,9 +2,18 @@ require 'json'
 require 'rubygems'
 require 'rack'
 
+SERVER_FILE = 'server.rb'
+
 class App
+	attr_accessor :last_mtime
+
 	def call(env)
-		load 'server.rb'
+		mtime = File.mtime(SERVER_FILE)
+
+		if last_mtime == nil or (last_mtime and mtime - last_mtime > 0)
+			load SERVER_FILE
+			last_mtime = mtime 
+		end
 
 		server_call env
 	end
--- server.rb
+++ server.rb
@@ -1,4 +1,4 @@
-puts "loading server... #yolo"
+puts "loading server..."
 
 require 'json'
 require "net/http"
@@ -16,172 +16,175 @@ rescue => e
     puts "failed #{e}"
 end
 
-@@tasks = {}
-@@filters = []
-
-def task_ok task
-	{
-		task: task["task"],
-		id: task["id"],
-		status: :success
-	}
-end
-
-def task name, &lambda
-	@@tasks[name.to_s] = lambda
-end
-
-def call_hook task, results={}
-	callback = task["callback"]
-
-	if callback
-		params = task["callback_params"]
-		unless params
-			params = task.clone
-		 	params.delete("callback")
-		 	taskname = params.delete("task")
-		 	params["original-task"] = taskname
-		end 
-
-		params[:results] = results
-
-		call_post callback, params
-	end
-end
-
-def load_tasks!
+module Worker
 	@@tasks = {}
-	load 'tasks.rb'	
-end
+	@@filters = []
 
-def list_tasks 
-	[
-		200,
-	 	{
-	 		"Content-Type" => "text/json"
-	 	},
-	 	[JSON.pretty_generate(@@tasks.keys)]
-	]
-end
-
-def page_not_found 
-	[404, {}, ["404 - Page not found"]]
-end
-
-def error_501 status
-	[
-		501, 
+	def self.task_ok task
 		{
-	 		"Content-Type" => "text/json"
-	 	},
-	 	[JSON.pretty_generate(status)] 
-	]
-end
-
-def work_or_501 error
-	begin
-		yield
-	rescue Exception => e 
-		puts "Error: #{e}\n#{e.backtrace.join("\n")}"
-		error_501 error
+			task: task["task"],
+			id: task["id"],
+			status: :success
+		}
 	end
-end
 
-def undefined_task_proc 
-	-> (params) {
-		{
-	 		message: "Undefined task!"
- 		}
-	}
-end
+	def self.task name, &lambda
+		@@tasks[name.to_s] = lambda
+	end
 
-def filter name, validator, &lambda
-	@@filters << {
-		validator: validator,
-		lambda: lambda
-	}
-end
+	def self.call_hook task, results={}
+		callback = task["callback"]
 
-def proc_for task
-	proc = @@tasks[task["task"]] 
+		if callback
+			params = task["callback_params"]
+			unless params
+				params = task.clone
+			 	params.delete("callback")
+			 	taskname = params.delete("task")
+			 	params["original-task"] = taskname
+			end 
 
-	unless proc
-		@@filters.each do |filter|
-			if filter[:validator].call(task)
-				proc = filter[:lambda]
-				break
-			end
+			params[:results] = results
+
+			call_post callback, params
 		end
 	end
 
-	proc ||= undefined_task_proc
+	def self.load_tasks!
+		@@tasks = {}
+		load 'tasks.rb'	
+	end
 
-	proc
-end
+	def self.list_tasks 
+		[
+			200,
+		 	{
+		 		"Content-Type" => "text/json"
+		 	},
+		 	[JSON.pretty_generate(@@tasks.keys)]
+		]
+	end
 
-def run task
-	name = task["task"]
+	def self.page_not_found 
+		[404, {}, ["404 - Page not found"]]
+	end
 
-	proc = proc_for task
+	def self.error_501 status
+		[
+			501, 
+			{
+		 		"Content-Type" => "text/json"
+		 	},
+		 	[JSON.pretty_generate(status)] 
+		]
+	end
 
-	output = proc.call(task)
+	def self.work_or_501 error
+		begin
+			yield
+		rescue Exception => e 
+			puts "Error: #{e}\n#{e.backtrace.join("\n")}"
+			error_501 error
+		end
+	end
 
-	[
-		200,
-	 	{
-	 		"Content-Type" => "text/json"
-	 	},
-	 	[JSON.pretty_generate(output)]
-	]
-end
+	def self.undefined_task_proc 
+		-> (params) {
+			{
+		 		message: "Undefined task!"
+	 		}
+		}
+	end
 
-load_tasks!
+	def self.filter name, validator, &lambda
+		@@filters << {
+			validator: validator,
+			lambda: lambda
+		}
+	end
 
-def server_call(env)
-	request = Rack::Request.new env
+	def self.proc_for task
+		proc = @@tasks[task["task"]] 
 
-	puts "#{request.request_method} #{request.path}"
-	# puts "params = #{request.params}" 
-
-	if request.path == "/tasks.json"
-		list_tasks
-	elsif request.path == "/run.json"
-		if request.request_method == "GET"
-			[
-				200,
-			 	{
-			 		"Content-Type" => "text/html"
-			 	},
-			 	[open('index.html').read]
-			]
-		elsif request.request_method == "POST"
-			work_or_501({ status: :error }) do 
-				
-				puts "=" * 100
-				puts "new task:"
-
-				params_info = request.params["params"] 
-
-				params = nil
-				if params_info == nil
-					params = JSON.parse(request.body.read)
-				else
-					params = JSON.parse(params_info)
+		unless proc
+			@@filters.each do |filter|
+				if filter[:validator].call(task)
+					proc = filter[:lambda]
+					break
 				end
-
-				id = SecureRandom.base64.to_s
-				params["id"] = id
-
-				puts "params = #{JSON.pretty_generate(params)}"
-
-				run(params)
 			end
-		else 				
+		end
+
+		proc ||= undefined_task_proc
+
+		proc
+	end
+
+	def self.run task
+		name = task["task"]
+
+		proc = proc_for task
+
+		output = proc.call(task)
+
+		[
+			200,
+		 	{
+		 		"Content-Type" => "text/json"
+		 	},
+		 	[JSON.pretty_generate(output)]
+		]
+	end
+
+	load_tasks!
+
+	def self.server_call(env)
+		request = Rack::Request.new env
+
+		puts "#{request.request_method} #{request.path}"
+		# puts "params = #{request.params}" 
+
+		if request.path == "/tasks.json"
+			list_tasks
+		elsif request.path == "/run.json"
+			if request.request_method == "GET"
+				[
+					200,
+				 	{
+				 		"Content-Type" => "text/html"
+				 	},
+				 	[open('index.html').read]
+				]
+			elsif request.request_method == "POST"
+				work_or_501({ status: :error }) do 
+					
+					puts "=" * 100
+					puts "new task:"
+
+					params_info = request.params["params"] 
+
+					params = nil
+					if params_info == nil
+						params = JSON.parse(request.body.read)
+					else
+						params = JSON.parse(params_info)
+					end
+
+					id = SecureRandom.base64.to_s
+					params["id"] = id
+
+					puts "params = #{JSON.pretty_generate(params)}"
+
+					run(params)
+				end
+			else 				
+				page_not_found
+			end
+		else
 			page_not_found
 		end
-	else
-		page_not_found
 	end
 end
 
+include Worker
 
