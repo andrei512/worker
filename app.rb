07c3cf2659df4d1d1c67c079384eb0ecfa9968ce
Andrei Puni <puni.andrei23@gmail.com>
2014-05-31 03:28:18 +0300

    forced reloading

--- app.rb
+++ app.rb
@@ -5,23 +5,8 @@ require 'rack'
 SERVER_FILE = 'server.rb'
 
 class App
-	attr_accessor :last_mtime
-
-
 	def call(env)
-		mtime = Dir.glob("**/*").inject(Time.new(0)) { |mem, var|  
-			time = File.mtime(var)
-			mem - time > 0 ? mem : time
-		}
-
-		@last_mtime ||= Time.new(0)
-
-		puts "[#{mtime - @last_mtime}] mtime: #{mtime} @last_mtime: #{@last_mtime}"
-
-		if mtime - @last_mtime > 0
-			load SERVER_FILE
-			@last_mtime = mtime 
-		end
+		load SERVER_FILE
 
 		Worker.server_call env
 	end
