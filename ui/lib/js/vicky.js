ba554a88c284a96382a3b549be186d2fa8592fb7
dhasegan <danielhasegan@gmail.com>
2014-07-20 17:32:52 -0400

    Cleaned up the client id and the api call for youtube search

--- ui/lib/js/vicky.js
+++ ui/lib/js/vicky.js
@@ -1,49 +1,18 @@
-var clientId = '598122546966-aeoj24o1qr5gqg6mmqfq4e5k51spddcl.apps.googleusercontent.com';
 
 var apiKey = 'AIzaSyCTeuyuYjzxpI73pRI1TcZTOA05EykD1fk';
 
-var scopes = 'https://www.googleapis.com/auth/youtube.readonly';
-
 function handleClientLoad() {
-    debugger
-  // Step 2: Reference the API key
   gapi.client.setApiKey(apiKey);
-  window.setTimeout(checkAuth,1);
 }
 
-function checkAuth() {
-    debugger
-  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
-}
-
-function handleAuthResult(authResult) {
-    debugger
-  var authorizeButton = document.getElementById('authorize-button');
-  console.log(authResult)
-  if (authResult && !authResult.error) {
-    authorizeButton.style.visibility = 'hidden';
-    makeApiCall();
-  } else {
-    authorizeButton.style.visibility = '';
-    authorizeButton.onclick = handleAuthClick;
-  }
-}
-
-function handleAuthClick(event) {
-  // Step 3: get authorization to use private data
-  gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
-  return false;
-}
-
-// Load the API and make an API call.  Display the results on the screen.
 function makeApiCall() {
-  // Step 4: Load the Google+ API
   gapi.client.load('youtube', 'v3', function() {
-    // Step 5: Assemble the API request
     var request = gapi.client.youtube.search.list({
       'q': 'tool',
       'part': 'snippet',
+      'fields': 'items(id,snippet)'
     });
+
     // Step 6: Execute the API request
     request.execute(function(resp) {
     // var heading = document.createElement('h4');
