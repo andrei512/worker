461a7d101398f758ded92f18f4d6a9d64557ea2f
dhasegan <danielhasegan@gmail.com>
2014-07-21 18:22:36 -0400

    Youtube js search work in progress

--- rails-app/app/assets/javascripts/application.js
+++ rails-app/app/assets/javascripts/application.js
@@ -16,4 +16,69 @@
 //= require turbolinks
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+$(function(){
+    $(document).foundation();
+
+    $(window).load( function() {
+
+        // Load Google API key
+        var apiKey = 'AIzaSyD9b0JYMU2vbjmNGhNyhYXZflQMjMU5aPs';
+        gapi.client.setApiKey(apiKey);
+    });
+});
+
+var LINK = "http://andrei512.no-ip.biz/run.json"
+
+function makeYoutubeCall(input, callback) {
+    gapi.client.load('youtube', 'v3', function() {
+        var request = gapi.client.youtube.search.list({
+            'q': input,
+            'part': 'snippet',
+            'fields': 'items(id,snippet)'
+        });
+
+        request.execute(callback);
+    });
+}
+
+var suggestionLinkClick = function(event) {
+    event.preventDefault();
+    $.ajax( this.href, {
+        method: 'post',
+        data: $(this).attr('value'),
+        success: function() {
+        },
+        error: function() {
+            console.log("Error")
+        }
+    })
+}
+
+$('#youtube-input').bind("enterKey",function(e){
+    var input = this.value
+    var params = {
+        'task': "youtube"
+    }
+    makeYoutubeCall(input, function(response) {
+        if (!response || response === undefined) {
+            return undefined;
+        }
+
+        html = "";
+        console.log(response)
+        $(response.items).each( function() {
+            params['message'] = this.snippet.title;
+            var vals = "params=" + JSON.stringify(params);
+
+            html += "<li> <a class='suggestion-link' href='" + LINK + "' value='" + vals + "'>" + this.snippet.title + "</a></li>";
+        });
+        $('#youtube-results').html(html);
+        $('.suggestion-link').click(suggestionLinkClick);
+    });
+});
+$('#youtube-input').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
